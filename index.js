be2f8a0f1e4d662b0ce0b5c9dbd5c2d43f6a4f72
maseeek <maseeekyt@gmail.com>
2025-02-10 13:26:09 +0000

    Done shi

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Happy developing ✨')
+
+function GetFile(){
+    var file = document.getElementById('file').files[0];
+    var reader = new FileReader();
+    reader.onload = function(e){
+        var text = reader.result;
+        console.log(text);
+    }
+    reader.readAsText(file);
+}
